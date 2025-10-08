264ff23dddebd3a9e6a1d1b87819102d69038d24
carloshg12 <carhergar@alu.edu.gva.es>
2025-10-08 10:50:44 +0200

    Ejercicios 1 a 3 hechos

--- Practica1.java
+++ Practica1.java
@@ -30,14 +30,63 @@ public class Practica1 {
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+        int elemento, dato;
+        Set<Integer> datos = new HashSet<>();
+        Set<Integer> sicuadrados = new HashSet<>();
+        datos.addAll(cuadrados);
+        datos.addAll(noCuadrados);
+        Iterator<Integer> itcuadrados = cuadrados.iterator();
+        Iterator<Integer> itnocuadrados = noCuadrados.iterator();
+        while (itcuadrados.hasNext()){
+            elemento=itcuadrados.next();
+            if(datos.contains(elemento*elemento))
+                sicuadrados.add(elemento*elemento);
+        }
+        while (itnocuadrados.hasNext()){
+            elemento=itnocuadrados.next();
+            if(datos.contains(elemento*elemento))
+                sicuadrados.add(elemento*elemento);
+        }
+        if(cuadrados.contains(1) && noCuadrados.contains(1))
+            sicuadrados.add(1);
+        else sicuadrados.remove(1);
+        cuadrados.removeAll(cuadrados);
+        cuadrados.addAll(sicuadrados);
+        noCuadrados.removeAll(noCuadrados);
+        datos.removeAll(sicuadrados);
+        noCuadrados.addAll(datos);
+
+        //arreglado
 
     }
 
     //EJERCICIO 3
-    public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
-        //TODO
-        return null;
+    public static <T> Collection<Set<T>> divideInSets(Iterator<T> it) {
+        List<Set<T>> result = new ArrayList<>();
+
+        // Procesar cada elemento del iterador
+        while (it.hasNext()) {
+            T element = it.next();
+            boolean added = false;
+
+            // Intentar agregar el elemento al primer conjunto donde no cause duplicación
+            for (Set<T> set : result) {
+                if (!set.contains(element)) {
+                    set.add(element);
+                    added = true;
+                    break;
+                }
+            }
+
+            // Si no se pudo agregar a ningún conjunto existente, crear uno nuevo
+            if (!added) {
+                Set<T> newSet = new HashSet<>();
+                newSet.add(element);
+                result.add(newSet);
+            }
+        }
+
+        return result;
     }
 
     //EJERCICIO 4
