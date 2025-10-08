2c08caf8b1e568dc6a76b1e2618b44fc03f266c2
carloshg12 <carhergar@alu.edu.gva.es>
2025-10-08 09:48:25 +0200

    Ejericio 1

--- Practica1.java
+++ Practica1.java
@@ -6,9 +6,27 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        List<Integer> lista = new ArrayList<>();
+        while (it.hasNext()) {
+            int num = it.next();
+            if (num != 0) {
+                lista.add(num);
+            }
+        }
+
+        Set<Integer> resultado = new HashSet<>();
+
+        for (int i = 0; i < lista.size(); i++) {
+            for (int j = 0; j < lista.size(); j++) {
+                if (i != j && lista.get(i) % lista.get(j) == 0) {
+                    resultado.add(lista.get(i));
+                }
+            }
+        }
+
+        return resultado;
     }
+
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
